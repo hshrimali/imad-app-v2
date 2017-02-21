31ce7c6b8c9035c0c510618232b20bc5764a2864
Himanshu Shrimali <hs30897@gmail.com>
2017-02-21 15:23:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     user: 'hshrimali',
     database: 'hshrimali',
     host: 'db.imad.hasura-app.io',
-    port: '5432',
+    port: 'localhost:5432',
     password: process.env.DB_PASSWORD
 };
 
